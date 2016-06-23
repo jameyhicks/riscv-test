eefea2a94b4e2f530a0dbe817af5a447731203bc
acw1251 <acwright@mit.edu>
2016-06-23 14:36:05 -0400

    updated runtest.sh

--- procs/RV64G_inorder/runtest.sh
+++ procs/RV64G_inorder/runtest.sh
@@ -12,7 +12,7 @@ if [ "$#" -eq 0 ] ; then
     echo "3) rv64uf-p-*"
     echo "4) rv64mi-p-*"
     echo "5) rv64si-p-*"
-    echo "6) rv64mi-p-dirty"
+    echo "6) rv64ui-p-amoadd_d"
     echo "7) linux"
     read OPTION
 else
@@ -36,7 +36,7 @@ case "$OPTION" in
        ;;
     5) files=`find $RISCV/riscv64-unknown-elf/share/riscv-tests/isa/rv64si-p-* -type f ! -name "*.*"`
        ;;
-    6) files=$RISCV/riscv64-unknown-elf/share/riscv-tests/isa/rv64mi-p-dirty
+    6) files=$RISCV/riscv64-unknown-elf/share/riscv-tests/isa/rv64ui-p-amoadd_d
        ;;
     7) $RUNEXE +ramdisk=$RISCV_HOME/root.bin $RISCV/riscv64-unknown-elf/bin/bbl $RISCV_HOME/vmlinux > out/linux.out
        files=
@@ -52,10 +52,20 @@ for hexfile in $files ; do
     # check return value
     errorcode=$?
     if [ $errorcode -ne 0 ] ; then
-        echo "$basehexfile FAILED $errorcode"
+        grep ERROR out/${basehexfile}.out > /dev/null
+        if [ $errorcode -ne 0 ] ; then
+            echo "$basehexfile FAILED $errorcode" # with divergence
+        else
+            echo "$basehexfile FAILED $errorcode (without divergence)"
+        fi
         # exit 1
     else
-        echo "$basehexfile OK"
+        grep ERROR out/${basehexfile}.out > /dev/null
+        if [ $errorcode -ne 0 ] ; then
+            echo "$basehexfile PASSED (with divergence)"
+        else
+            echo "$basehexfile OK"
+        fi
     fi
 done
 rm SOCK.*
