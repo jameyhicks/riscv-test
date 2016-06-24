92eb6786435b7d3d418f0f9a0e52a3ded39ccbf2
Jamey Hicks <jamey.hicks@gmail.com>
2016-06-24 12:53:19 -0400

    build for verilator

---
+++ build-verilator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export RISCV_HOME=$PWD
+export RISCV_BUILD=$RISCV_HOME/build
+export RISCV_TOOLS=$RISCV_HOME/riscv
+export RISCV=$RISCV_TOOLS
+
+export PATH=$RISCV/bin:$PATH
+export LD_LIBRARY_PATH=$RISCV/lib
+
+(cd riscv-tools; ./build.sh) || exit 1
+#(cd procs/RV64G_multicycle; make gen.verilator && make -j8 -C verilator bits) || exit 2
+#(cd procs/RV64G_multicycle; make -C verilator exe) || exit 3
+#cd procs; ./runtests.sh
