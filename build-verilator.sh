ec8a01f6313eccee7e8c88cc539ffeb99299cb79
Jamey Hicks <jamey.hicks@gmail.com>
2016-06-27 15:21:44 -0400

    build the riscv-unknown-linux-gnu toolchain

--- build-verilator.sh
+++ build-verilator.sh
@@ -9,6 +9,7 @@ export PATH=$RISCV/bin:$PATH
 export LD_LIBRARY_PATH=$RISCV/lib
 
 (cd riscv-tools; ./build.sh) || exit 1
+(cd riscv-tools; make -C riscv-gnu-toolchain/build -j2 linux) || exit 1
 #(cd procs/RV64G_multicycle; make gen.verilator && make -j8 -C verilator bits) || exit 2
 #(cd procs/RV64G_multicycle; make -C verilator exe) || exit 3
 #cd procs; ./runtests.sh
