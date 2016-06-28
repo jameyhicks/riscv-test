7d9759543d98f4a6996d9e469e374f0e8ab32a4b
Jamey Hicks <jamey.hicks@gmail.com>
2016-06-27 20:50:09 -0400

    half undo previous change

--- build-verilator.sh
+++ build-verilator.sh
@@ -9,7 +9,7 @@ export PATH=$RISCV/bin:$PATH
 export LD_LIBRARY_PATH=$RISCV/lib
 
 (cd riscv-tools; ./build.sh > /dev/null) || exit 1
-(cd riscv-tools; make -C riscv-gnu-toolchain/build -j2 linux > /dev/null)
+(cd riscv-tools; make -C riscv-gnu-toolchain/build -j2 linux)
 #(cd procs/RV64G_multicycle; make gen.verilator && make -j8 -C verilator bits) || exit 2
 #(cd procs/RV64G_multicycle; make -C verilator exe) || exit 3
 #cd procs; ./runtests.sh
