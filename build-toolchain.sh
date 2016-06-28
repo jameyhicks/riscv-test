04bda9c846fcc137da461b01ad95c69baa069dd9
Jamey Hicks <jamey.hicks@gmail.com>
2016-06-28 12:12:01 -0400

    build-toolchain.sh to customize build-tools/build.sh

---
+++ build-toolchain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export RISCV_HOME=$PWD
+export RISCV_BUILD=$RISCV_HOME/build
+export RISCV_TOOLS=$RISCV_HOME/riscv
+export RISCV=$RISCV_TOOLS
+
+export PATH=$RISCV/bin:$PATH
+export LD_LIBRARY_PATH=$RISCV/lib
+
+rm -fr riscv
+
+(cd riscv-tools; git clean -fdx; \
+ . build.common.sh; \
+ build_project riscv-gnu-toolchain --prefix=$RISCV --enable-linux --disable-multilib --with-xlen=64 --with-arch=IMAFD ; \
+)
+ls riscv/bin
+#(cd procs/RV64G_multicycle; make gen.verilator && make -j8 -C verilator bits) || exit 2
+#(cd procs/RV64G_multicycle; make -C verilator exe) || exit 3
+#cd procs; ./runtests.sh
