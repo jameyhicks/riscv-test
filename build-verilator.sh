f3e0b703e5a29c42a10276765839e9b1083e7c8b
Jamey Hicks <jamey.hicks@gmail.com>
2016-06-28 11:41:51 -0400

    use --enable-linux to build the linux toolchain

--- build-verilator.sh
+++ build-verilator.sh
@@ -8,8 +8,14 @@ export RISCV=$RISCV_TOOLS
 export PATH=$RISCV/bin:$PATH
 export LD_LIBRARY_PATH=$RISCV/lib
 
-(cd riscv-tools; ./build.sh > /dev/null) || exit 1
-(cd riscv-tools; make -C riscv-gnu-toolchain/build -j2 linux > /dev/null) 
+(cd riscv-tools; . build.common; \
+echo "Starting RISC-V Toolchain build process"; \
+build_project riscv-fesvr --prefix=$RISCV; \
+build_project riscv-isa-sim --prefix=$RISCV --with-fesvr=$RISCV; \
+build_project riscv-gnu-toolchain --prefix=$RISCV --enable-linux; \
+CC= CXX= build_project riscv-pk --prefix=$RISCV/riscv64-unknown-elf --host=riscv64-unknown-elf; \
+build_project riscv-tests --prefix=$RISCV/riscv64-unknown-elf; \
+echo -e "\\nRISC-V Toolchain installation completed!")
 #(cd procs/RV64G_multicycle; make gen.verilator && make -j8 -C verilator bits) || exit 2
 #(cd procs/RV64G_multicycle; make -C verilator exe) || exit 3
 #cd procs; ./runtests.sh
